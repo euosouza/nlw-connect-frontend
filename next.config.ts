5ddea9a06912cb7a75d02fd6821083f82fb6ccb4
Victor Souza <victor.souza@bulbe.com>
2025-02-20 22:56:39 -0300

    chore: add url base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/nlw-connect-frontend",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
